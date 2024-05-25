832a39b1436d8ccef305a976ae32165fa8775b87
hendisantika <hendisantika@yahoo.co.id>
2024-05-25 09:26:21 +0700

    Add Client Model 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootreactapp/model/Client.java
@@ -0,0 +1,34 @@
+package id.my.hendisantika.springbootreactapp.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : spring-boot-react-app
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 25/05/24
+ * Time: 09.25
+ * To change this template use File | Settings | File Templates.
+ */
+@Entity
+@Table(name = "client")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Client {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String name;
+    private String email;
+}
