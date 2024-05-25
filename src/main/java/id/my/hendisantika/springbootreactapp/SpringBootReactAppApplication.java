a7382132e526220ab81d0c350d6896cb6b4b7996
hendisantika <hendisantika@yahoo.co.id>
2024-05-25 09:18:22 +0700

    Create Spring Boot App 🫸🌀✏️📗🐧🐳

---
+++ src/main/java/id/my/hendisantika/springbootreactapp/SpringBootReactAppApplication.java
@@ -0,0 +1,13 @@
+package id.my.hendisantika.springbootreactapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootReactAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootReactAppApplication.class, args);
+    }
+
+}
